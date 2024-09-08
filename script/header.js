4cfe3dbded8753e9024cca5671f3898b75c0f746
tarikku_desu <amehritarik@gmail.com>
2024-09-08 21:57:52 +0100

    changed the header a little and added a drop down menu

--- script/header.js
+++ script/header.js
@@ -1,2 +1,17 @@
 'use strict'
 
+const dropDownMenu = document.getElementById(`drop-down-menu`);
+const hamBtn = document.getElementById(`ham-icon`);
+const closeBtn = document.getElementById(`close-icon`);
+
+hamBtn.addEventListener('click', (e) => {
+    hamBtn.classList.toggle(`hidden`);
+    closeBtn.classList.toggle(`hidden`);
+    dropDownMenu.classList.toggle(`hidden`);
+});
+
+closeBtn.addEventListener('click', (e) => {
+    hamBtn.classList.toggle(`hidden`);
+    closeBtn.classList.toggle(`hidden`);
+    dropDownMenu.classList.toggle(`hidden`);
+});
