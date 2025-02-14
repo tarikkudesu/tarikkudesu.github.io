3b649d49c48cb4a74f8c3b6137fdd83a954caebf
tarikku_desu <amehritarik@gmail.com>
2025-02-14 20:29:08 +0100

    first step into web developement

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+function Header() {
+  return <header className="header"></header>;
+}
+
+export default App;
